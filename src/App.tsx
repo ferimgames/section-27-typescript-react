0095f35111d18210df558f6b34bc88f2ea9bcdef
ferimgames <116451280+ferimgames@users.noreply.github.com>
2023-05-11 11:55:42 -0400

    adding generic component with TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
+import Todos from "./components/Todo";
+
+const DUMMY_ITEMS = ["Learn React", "Learn JS", "Learn TW"];
 
 function App() {
   return (
     <div className="App">
-
+      <Todos items={DUMMY_ITEMS} />
     </div>
   );
 }
